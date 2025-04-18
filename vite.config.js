61b6d1ba837616415067548b9e3f47bd36a48f7d
Ruslan Mykytyn <ruslan.mykytyn.03@gmail.com>
2025-04-18 13:55:58 +0300

    Prepare dist for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
+        main: resolve(__dirname, "main.html"),
         categories: resolve(__dirname, "categories.html"),
         contact: resolve(__dirname, "contact.html"),
       },
